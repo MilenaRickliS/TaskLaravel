c2d995bd9514c3ec400188c9c418d007cdd784b2
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2024-11-02 14:54:06 -0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 
+Route::get('/', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('/', 'Auth\LoginController@login');
+Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('/register', 'Auth\RegisterController@register');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
@@ -13,3 +21,6 @@ Route::middleware(['auth'])->group(function () {
     Route::patch('/tasks/{id}', [TaskController::class, 'update'])->where('id', '[0-9]+')->name('tasks.update');
     Route::delete('/tasks/{id}', [TaskController::class, 'destroy'])->where('id', '[0-9]+')->name('tasks.destroy');
 });
+
+
+
